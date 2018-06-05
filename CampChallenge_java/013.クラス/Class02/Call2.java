d4eb818f542a202d0bdd4ca5ea1c86c32284e594
DESKTOP-OJ1PQ18\sumi3 <catenaccio.1982@gmail.com>
2018-06-05 12:04:59 +0900

    クラス課題02「クラスの継承」

---
+++ CampChallenge_java/013.クラス/Class02/Call2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class02;
+
+/**
+ *
+ * @author sumi3
+ */
+public class Call2 {
+    public static void main (String[] args){
+        Non mono = new Non();
+        mono.setPen("赤",22);
+        mono.getPen();
+        mono.clear();
+        mono.getPen();
+    }
+    
+}
---
+++ CampChallenge_java/013.クラス/Class02/Non.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class02;
+
+/**
+ *
+ * @author sumi3
+ */
+public class Non extends Pen{
+    public void clear(){
+        this.color = null;
+        this.length = 0;
+    }
+}
