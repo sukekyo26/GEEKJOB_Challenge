5e5147f9b995ae5f41a5ccb1e8e250a1da80569d
DESKTOP-OJ1PQ18\sumi3 <catenaccio.1982@gmail.com>
2018-06-05 11:36:07 +0900

    クラス課題01「クラスの作成」

---
+++ CampChallenge_java/013.クラス/Class01/Call.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class01;
+
+/**
+ *
+ * @author sumi3
+ */
+public class Call {
+    public static void main(String[] args){
+        Pen ka = new Pen();
+        ka.setPen("緑", 15);
+        ka.getPen();
+    }
+    
+}
---
+++ CampChallenge_java/013.クラス/Class01/Pen.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class01;
+
+/**
+ *
+ * @author sumi3
+ */
+public class Pen {
+    public String color;
+    public int length;
+    
+    public void setPen(String a, int b){
+        this.color = a;
+        this.length = b;
+    }
+    
+    public void getPen(){
+        System.out.println(this.color);
+        System.out.println(this.length);
+    }    
+}
