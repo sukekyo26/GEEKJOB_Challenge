f222f129b05d7b322cc7372c8799785c375b631a
DESKTOP-OJ1PQ18\sumi3 <catenaccio.1982@gmail.com>
2018-06-07 14:13:30 +0900

    標準クラス課題05「タイムスタンプの表示」

---
+++ CampChallenge_java/015.標準クラス/StandardClass05/StandardClass05.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StandardClass05;
+
+/**
+ *
+ * @author sumi3
+ */
+public class StandardClass05 {
+    public static void main(String[] args){
+        String myName = "小泉丞巨";
+        System.out.println("バイト数:" + myName.getBytes().length);
+        System.out.println("文字数:" + myName.length());
+    }    
+}
