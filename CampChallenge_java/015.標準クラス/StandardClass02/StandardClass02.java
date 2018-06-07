c87e4badebc71d9622029df8545e51469ef970a3
DESKTOP-OJ1PQ18\sumi3 <catenaccio.1982@gmail.com>
2018-06-07 12:42:09 +0900

    標準クラス課題02「現在の日時の表示」

---
+++ CampChallenge_java/015.標準クラス/StandardClass02/StandardClass02.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StandardClass02;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+/**
+ *
+ * @author sumi3
+ */
+public class StandardClass02 {
+    public static void main(String[] args){
+        // 今日の日付
+        Date now = new Date();
+        // SimpleDateFormat
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日 HH時mm分ss秒");
+
+        System.out.print(sdf.format(now));
+    }
+}
