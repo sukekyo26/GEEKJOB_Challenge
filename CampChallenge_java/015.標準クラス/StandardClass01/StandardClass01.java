460a00b304129a9b7bd73c07717c42950429b5ed
DESKTOP-OJ1PQ18\sumi3 <catenaccio.1982@gmail.com>
2018-06-07 12:15:32 +0900

    標準クラス課題01「タイムスタンプの作成」

---
+++ CampChallenge_java/015.標準クラス/StandardClass01/StandardClass01.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StandardClass01;
+import java.util.Date;
+import java.util.Calendar;
+/**
+ *
+ * @author sumi3
+ */
+public class StandardClass01 {
+
+    public static void main(String[] args){
+        Calendar a = Calendar.getInstance();
+        //2016年1月1日 0時0分0秒
+        a.set(2016, 0, 1, 00, 00, 00);
+        System.out.println(a.getTime());
+    }
+}    
+
