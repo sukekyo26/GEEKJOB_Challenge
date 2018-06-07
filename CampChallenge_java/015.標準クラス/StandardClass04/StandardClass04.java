cf914982fcf93588ba4da53471e7f5fbaf3fb22c
DESKTOP-OJ1PQ18\sumi3 <catenaccio.1982@gmail.com>
2018-06-07 14:14:18 +0900

    k

---
+++ CampChallenge_java/015.標準クラス/StandardClass04/StandardClass04.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StandardClass04;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+/**
+ *
+ * @author sumi3
+ */
+public class StandardClass04 {
+    public static void main(String[] args){
+        Calendar a = Calendar.getInstance();
+        Calendar b = Calendar.getInstance();
+        a.set(2015, 0, 1, 0, 0, 0);
+        b.set(2015, 11, 31, 23, 59, 59);
+        Date c = a.getTime(); 
+        Date d = b.getTime();
+        Date e = d - c;
+        
+        System.out.println();
+                
+    }    
+}
