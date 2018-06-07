de968e434bfff8bd9e11427c2e56ecfa887aa9bb
DESKTOP-OJ1PQ18\sumi3 <catenaccio.1982@gmail.com>
2018-06-07 13:12:09 +0900

    標準クラス課題03「タイムスタンプの表示」

---
+++ CampChallenge_java/015.標準クラス/StandardClass03/StandardClass03.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StandardClass03;
+import java.text.SimpleDateFormat;
+import java.util .Calendar;
+import java.util.Date;
+
+/**
+ *
+ * @author sumi3
+ */
+public class StandardClass03 {
+    public static void main(String[] args){
+        Calendar c = Calendar.getInstance();
+        c.set(2016, 10, 4, 10, 0, 0);
+        
+        System.out.println(c.getTime());
+        
+        Date d = c.getTime();
+        SimpleDateFormat sdf =new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.print(sdf.format(d));
+    }    
+}
