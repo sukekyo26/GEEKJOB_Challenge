ce1a3cb809bf126343a72bf579c7b52747613312
DESKTOP-OJ1PQ18\sumi3 <catenaccio.1982@gmail.com>
2018-06-07 14:37:46 +0900

    標準クラス課題07「文字の入れ替え」

---
+++ CampChallenge_java/015.標準クラス/StandardClass07/StandardClass07.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StandardClass07;
+
+/**
+ *
+ * @author sumi3
+ */
+public class StandardClass07 {
+    public static void main(String[] args){
+        String set = "きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
+        System.out.print(set.replace("I", "い").replace("U", "う"));
+    }    
+}
