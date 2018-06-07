eb86028e5c59b50ffeb5f5bcf373fa13c2889c6c
DESKTOP-OJ1PQ18\sumi3 <catenaccio.1982@gmail.com>
2018-06-07 14:20:30 +0900

    標準クラス課題06「文字数の取得」

---
+++ CampChallenge_java/015.標準クラス/StandardClass06/StandardClass06.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StandardClass06;
+
+/**
+ *
+ * @author sumi3
+ */
+public class StandardClass06 {
+    public static void main(String[] args){
+        String myMailAddress = "mymailaddress@gmail.com";
+        System.out.print(myMailAddress.substring(13));
+    }    
+}
