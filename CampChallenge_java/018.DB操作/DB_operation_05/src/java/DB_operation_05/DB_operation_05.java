8037de98bf84eb078275faccaa2d3f295d808909
DESKTOP-OJ1PQ18\sumi3 <catenaccio.1982@gmail.com>
2018-07-27 12:08:39 +0900

    DB操作課題05「特定のレコードの情報取得と表示」

---
+++ CampChallenge_java/018.DB操作/DB_operation_05/src/java/DB_operation_05/DB_operation_05.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DB_operation_05;
+import java.sql.*;
+/**
+ *
+ * @author sumi3
+ */
+public class DB_operation_05 {
+    public static void main (String[] args){
+        Connection db_co = null;
+        PreparedStatement db_ps = null;
+        ResultSet db_rs = null;
+        
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            db_co = DriverManager.getConnection("jdbc:mysql://localhost:3306/Challenge_db?CharacterEncording=UTF-8&serverTimezone=JST", "root", "");
+            db_ps = db_co.prepareStatement("SELECT * FROM profiles WHERE name LIKE ?");
+            db_ps.setString(1, "%茂%");
+            db_rs = db_ps.executeQuery();
+            while(db_rs.next()){
+                System.out.print("ID: " + db_rs.getString(1) + " ");
+                System.out.print("名前: " + db_rs.getString(2) + " ");
+                System.out.print("電話番号: " + db_rs.getString(3) + " ");
+                System.out.print("年齢: " + db_rs.getString(4) + " ");
+                System.out.println("誕生日: " + db_rs.getString(5) + " ");
+            }    
+            
+            db_co.close();
+            db_ps.close();
+            db_rs.close();
+        } catch(SQLException e_sql) {
+            System.out.println("接続時にエラーが発生しました：" + e_sql.toString());
+        } catch(Exception e) {
+            System.out.println("接続時にエラーが発生しました：" + e.toString());
+        } finally {
+            if (db_co != null){
+                try{
+                    db_co.close();
+                } catch(Exception e_co){    
+                    System.out.print(e_co.getMessage());
+                }    
+            }
+            if (db_ps != null){
+                try{
+                    db_ps.close();
+                } catch(Exception e_ps){    
+                    System.out.print(e_ps.getMessage());
+                }    
+            }
+            if (db_rs != null){
+                try{
+                    db_rs.close();
+                } catch(Exception e_rs){    
+                    System.out.print(e_rs.getMessage());
+                }    
+            }   
+        } 
+    }    
+}
